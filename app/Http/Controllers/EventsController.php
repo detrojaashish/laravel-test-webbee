5d695e1a2302c077e546d10a2ad10098f3a685db
Mohin Lakdawala <mohin.lakdawala@gmail.com>
2021-05-26 11:51:16 +0530

    Complete the getFutureEventsWithWorkshops function

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
 
 class EventsController extends BaseController
 {
@@ -176,6 +177,9 @@ class EventsController extends BaseController
      */
 
     public function getFutureEventsWithWorkshops() {
-        throw new \Exception('implement in coding task 2');
+        return Event::with('workshops')
+            ->select(['events.*', DB::raw('(SELECT MIN(START) FROM workshops WHERE event_id = events.id) AS workshop_min_start')])
+            ->havingRaw('workshop_min_start >= ?', [now()])
+            ->get();
     }
 }
